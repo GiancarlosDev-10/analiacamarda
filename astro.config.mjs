8ea716b85f8ab4bb6f6bf29b12e2d2e36dc891c5
Giancarlos Ormeño <giangio237@gmail.com>
2026-04-11 11:36:54 -0500

    Cambios textos

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "always",
+  trailingSlash: "ignore",
   site: "https://www.analiacamarda.com",
 
   vite: {
